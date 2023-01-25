5b558fc16b0a1ade120ab5d0d792ce3558298016
EC2 Default User <ec2-user@ip-172-31-1-9.us-west-1.compute.internal>
2023-01-25 17:34:16 +0000

    lowers number of iterations

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -23,6 +23,8 @@ struct IVPRequest {
 }
 
 const K: f64 = 6.6743;
+const CHAMBER_RADIUS_SQ: f64 = 9.;
+const PENALTY: f64 = 200.;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -60,12 +62,20 @@ fn integrate(req: IVPRequest) -> Vec<Point> {
         }
     };
     
+    let j = {
+        if req.field == "single_repulsor" {
+            j_origin_repulsor
+        } else {
+            j_origin_attractor
+        }
+    };
+    
     let mut trajectory: Vec<State> = Vec::new();
     trajectory.push(State::new(req.x0[0], req.x0[1], req.v0[0], req.v0[1]));
     
     for _ in 0..req.n {
         let s = trajectory.last().expect("This should be impossible");
-        trajectory.push(step(*s, field, req.h));
+        trajectory.push(step(*s, field, j, req.h));
     }
     
     trajectory.into_iter().map(|s| Point {x: Vector2::new(s[0], s[1]), v: Vector2::new(s[2], s[3])}).collect()
@@ -74,21 +84,6 @@ fn integrate(req: IVPRequest) -> Vec<Point> {
 fn f_origin_attractor(x: State) -> State {
     let d2 = x[0].powf(2.) + x[1].powf(2.);
     let force = -x * K / d2;
-    
-    State::new(x[2], x[3], force[0], force[1])
-}
-
-fn f_origin_repulsor(x: State) -> State {
-    let d2 = x[0].powf(2.) + x[1].powf(2.);
-    let penalty = {
-        if d2 > 9. {
-            (9. - d2) * 200.
-        } else {
-            0.
-        }
-    };
-    
-    let force = x * (K / d2 + penalty);
     
     State::new(x[2], x[3], force[0], force[1])
 }
@@ -110,16 +105,57 @@ fn j_origin_attractor(x: State, h: f64) -> Matrix4<f64> {
     j
 }
 
-fn step_fe(y: State, f: fn(State) -> State, h: Scalar) -> State {
+fn f_origin_repulsor(x: State) -> State {
+    let d2 = x[0].powf(2.) + x[1].powf(2.);
+    let penalty = {
+        if d2 > CHAMBER_RADIUS_SQ {
+            (CHAMBER_RADIUS_SQ - d2) * PENALTY
+        } else {
+            0.
+        }
+    };
+    
+    let force = x * (K / d2 + penalty);
+    
+    State::new(x[2], x[3], force[0], force[1])
+}
+
+fn j_origin_repulsor(x: State, h: f64) -> Matrix4<f64> {
+
+    // ad hoc computation steps
+    let r2 = x[0].powf(2.) + x[1].powf(2.);
+    let k = {
+        if r2 < CHAMBER_RADIUS_SQ {
+            -K
+        } else {
+            PENALTY - K
+        }
+    };
+
+    let denom = r2.powf(2.);
+    let diff = - h * k * (x[0].powf(2.) - x[1].powf(2.)) / denom;
+    let prod = - h * k * 2. * x[0] * x[1] / denom;
+    
+    let j = Matrix4::new(
+         -1.,    0.,  h,  0., 
+          0.,   -1.,  0.,  h,
+        diff,  prod, -1.,  0.,
+        prod, -diff,  0., -1.
+        );
+        
+    j
+}
+
+fn step_fe(y: State, f: fn(State) -> State, _j: fn(State, f64) -> Matrix4<f64>, h: Scalar) -> State {
     y + h * f(y)
 }
 
-fn step_be(y: State, f: fn(State) -> State, h: Scalar) -> State {
-    let mut approx = step_fe(y, f, h);
+fn step_be(y: State, f: fn(State) -> State, j: fn(State, f64) -> Matrix4<f64>, h: Scalar) -> State {
+    let mut approx = step_fe(y, f, j, h);
 
-    for _ in 0..1000 {
+    for _ in 0..100 {
     
-        let diff = j_origin_attractor(approx, h).lu().solve(&(y + h * f(approx) - approx));
+        let diff = j(approx, h).lu().solve(&(y + h * f(approx) - approx));
         match diff {
             Some(value) => { approx -= value;}
             None        => { return y; }
@@ -129,7 +165,7 @@ fn step_be(y: State, f: fn(State) -> State, h: Scalar) -> State {
     approx
 }
 
-fn step_rk4(y: State, f: fn(State) -> State, h: Scalar) -> State {
+fn step_rk4(y: State, f: fn(State) -> State, _j: fn(State, f64) -> Matrix4<f64>, h: Scalar) -> State {
     let k1 = f(y);
     let k2 = f(y + h * k1 / 2.);
     let k3 = f(y + h * k2 / 2.);
